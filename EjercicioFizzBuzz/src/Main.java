4d938da2684c8791c0a22671c7aeee2e147275c1
Arturo Landa Espinosa <61170842+artlandae@users.noreply.github.com>
2022-07-30 18:47:03 -0500

    Creación del ejercicio Fizz Buzz

---
+++ EjercicioFizzBuzz/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeros = 1;
+        int contador = 0;
+        int contadorCinco = 0;
+
+            while(numeros <= 100) {
+                int multiplo = 1;
+                if (numeros != multiplo) {
+                    for(; multiplo <= numeros; ++multiplo) {
+                        if (numeros == multiplo * 3) {
+                            contador = multiplo * 3;
+                        }
+                    }
+                }
+
+                multiplo = 1;
+                if (numeros != multiplo) {
+                    for(; multiplo <= numeros; ++multiplo) {
+                        if (numeros == multiplo * 5) {
+                            contadorCinco = multiplo * 5;
+                        }
+                    }
+                }
+
+                if (numeros == contador && numeros == contadorCinco) {
+                    System.out.println("FIZZBUZZ");
+                } else if (numeros == contador) {
+                    System.out.println("FIZZ");
+                } else if (numeros == contadorCinco) {
+                    System.out.println("BUZZ");
+                }
+
+                if (numeros != contador && numeros != contadorCinco) {
+                    System.out.println(numeros);
+                    ++numeros;
+                } else {
+                    ++numeros;
+                }
+            }
+
+            return;
+    }
+}
