7d2e31b224e22373effbf90aa7c60af4bf5864d6
artlandae <61170842+artlandae@users.noreply.github.com>
2022-08-02 20:33:43 -0500

    Ejercicio FIzzBuzz con funciones
    El mismo funcionamiento, aplicando funciones.

--- EjercicioFizzBuzz/src/Main.java
+++ EjercicioFizzBuzz/src/Main.java
@@ -1,44 +1,52 @@
 public class Main {
-    public static void main(String[] args) {
-        int numeros = 1;
-        int contador = 0;
-        int contadorCinco = 0;
+    static int multiploTres(int multiplo, int numeros){
 
-            while(numeros <= 100) {
-                int multiplo = 1;
-                if (numeros != multiplo) {
-                    for(; multiplo <= numeros; ++multiplo) {
-                        if (numeros == multiplo * 3) {
-                            contador = multiplo * 3;
-                        }
-                    }
-                }
-
-                multiplo = 1;
-                if (numeros != multiplo) {
-                    for(; multiplo <= numeros; ++multiplo) {
-                        if (numeros == multiplo * 5) {
-                            contadorCinco = multiplo * 5;
-                        }
-                    }
-                }
-
-                if (numeros == contador && numeros == contadorCinco) {
-                    System.out.println("FIZZBUZZ");
-                } else if (numeros == contador) {
-                    System.out.println("FIZZ");
-                } else if (numeros == contadorCinco) {
-                    System.out.println("BUZZ");
-                }
-
-                if (numeros != contador && numeros != contadorCinco) {
-                    System.out.println(numeros);
-                    ++numeros;
-                } else {
-                    ++numeros;
+        int contador =0;
+        if (numeros != multiplo) {
+            for(; multiplo <= numeros; ++multiplo) {
+                if (numeros == multiplo * 3) {
+                    contador = multiplo * 3;
                 }
             }
+        }
+        return contador;
+    }
 
-            return;
+    static int multiploCinco(int multiplo, int numeros){
+        int contadorCinco=0;
+        if (numeros != multiplo) {
+            for(; multiplo <= numeros; ++multiplo) {
+                if (numeros == multiplo * 5) {
+                    contadorCinco = multiplo * 5;
+                }
+            }
+        }
+        return contadorCinco;
+    }
+
+    static int FizzBuzz(int contador,int contadorCinco,int numeros){
+        if (numeros == contador && numeros == contadorCinco) {
+            System.out.println("FIZZBUZZ");
+        } else if (numeros == contador) {
+            System.out.println("FIZZ");
+        } else if (numeros == contadorCinco) {
+            System.out.println("BUZZ");
+        }
+        if (numeros != contador && numeros != contadorCinco) {
+            System.out.println(numeros);
+            ++numeros;
+        }
+        else {
+            numeros++;
+        }
+        return numeros;
+    }
+    public static void main(String[] args) {
+        int numeros = 1;
+
+        while(numeros <= 100) {
+            numeros=FizzBuzz(multiploTres(1,numeros),multiploCinco(1,numeros),numeros);
+        }
+        return;
     }
 }
